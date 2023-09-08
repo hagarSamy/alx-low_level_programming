337f04e9ce4738e1013e6e11de764baa8ea81aeb
hagarSamy <hagarsami63@gmail.com>
2023-09-08 16:59:15 -0700

    modulus

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,11 +2,10 @@
 #include <time.h>
 #include <stdio.h>
 
-/*
- * main - entry point
+/**
+ * main - Entry point
  *
- * Description: prints the values of n staus:
- *		greater than 5, is zero and is less than 6 and not 0
+ * testing number and printing specific output on conditions
  *
  * Return: Always (0) success
  */
@@ -18,14 +17,13 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	m = n % 10;
-
 	if (m > 5)
 	{
 		printf("Last digit of %d is %d and is greater than 5\n", n, m);
 	}
 	else if (m == 0)
 	{
-		printf("Last digit of %d is 0\n", n);
+		printf("Last digit of %d is 0 and is 0\n", n);
 	}
 	else
 	{
