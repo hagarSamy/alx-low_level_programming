dc1206498bec7fdda918889d2ff132c5ec9fb783
hagarSamy <hagarsami63@gmail.com>
2023-09-08 20:07:42 -0700

    printing hollows

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+	int i = 0, j;
+
+	while (i < 100)
+	{
+		j = 0;
+		while (j < 100)
+		{
+			if (i < j)
+			{
+				putchar((i / 10) + 48);
+				putchar((i % 10) + 48);
+				putchar(' ');
+				putchar((j / 10) + 48);
+				putchar((j % 10) + 48);
+				if (i == 98 && j == 99)
+				{
+					putchar('\n');
+				}
+				else
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+		i++;
+	}
+	i++;
+	return (0);
+}
