8ca4d36b6c3080389d1bf46a40f8907f8d570b3f
hagarSamy <hagarsami63@gmail.com>
2023-09-16 04:51:46 -0700

    hours and mins of the day

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,11 +8,19 @@
 
 void jack_bauer(void)
 {
-	int i. j;
+	int i, j;
 
 	for (i = 0; i < 24; i++)
 	{
 		for (j = 0; j < 60; j++)
-			printf("%02d:%02d\n" i, j);
+		{
+			_putchar((i / 10) + 48);
+			_putchar((i % 10) + 48);
+			_putchar(':');
+			_putchar((j / 10) + 48);
+			_putchar((j % 10) + 48);
+			_putchar('\n');
+		}
+
 	}
 }
