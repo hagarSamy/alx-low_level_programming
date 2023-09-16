64c65fc6f0c465e95116648a017d0e70ec2115e6
hagarSamy <hagarsami63@gmail.com>
2023-09-15 17:44:10 -0700

    check for alphabet

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _isalpha - checks for alphabetic character
+ * @c: integer input
+ * Return: (1) if c is a letter, (0) otherwise
+ */
+
+int _isalpha(int c)
+{
+	if ((c > 96 && c < 123) || (c > 65 && c < 91))
+		return (1);
+	else return (0);
+}
