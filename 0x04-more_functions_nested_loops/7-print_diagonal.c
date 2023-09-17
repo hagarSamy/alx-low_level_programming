fb6f0d7076a5f0baff61881c4b82fa64d7295bcb
hagarSamy <hagarsami63@gmail.com>
2023-09-17 13:11:22 -0700

    diagonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -14,6 +14,7 @@ void print_diagonal(int n)
 	if (n <= 0)
 		_putchar('\n');
 	else
+	{
 		for (space = 1; space <= n; space++)
 		{
 			for (postn = 1; postn <= space; postn++)
@@ -21,4 +22,5 @@ void print_diagonal(int n)
 			_putchar(92);
 			_putchar('\n');
 		}
+	}
 }
