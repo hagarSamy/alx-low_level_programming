e01dfdda550981eead86d1924280798dcaf8d445
hagarSamy <hagarsami63@gmail.com>
2023-09-22 10:24:14 -0700

    string length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen - calculates the length of a string
+ * @s: pointer to a string
+ * Retuen: number of bytes in string
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+		++i; /*because first count is 0 like its index*/
+	return (i);
+}
