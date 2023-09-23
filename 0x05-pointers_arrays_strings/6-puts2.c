35e7d005e22624643e70566a22d545751123e013
hagarSamy <hagarsami63@gmail.com>
2023-09-23 06:47:10 -0700

    printing one of each two

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -11,10 +11,10 @@ void puts2(char *str)
 {
 	int i;
 
-	for (i = 0; str [i] != '\0'; i++)
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		putchar(str[i]);
-		i++;
+		if (i % 2 == 0)
+			putchar(str[i]);
 	}
 	putchar('\n');
 }
