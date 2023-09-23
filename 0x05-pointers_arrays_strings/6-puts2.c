df8ffd50f4f0b9f0de0b1e3c6d8554fb39fdb73a
hagarSamy <hagarsami63@gmail.com>
2023-09-23 06:37:06 -0700

    printing one of each two

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a string
+ * starting with the first character, followed by a new line
+ * @str: string
+ * Return: void
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str [i] != '\0'; i++)
+	{
+		putchar(str[i]);
+		i++;
+	}
+	putchar('\n');
+}
