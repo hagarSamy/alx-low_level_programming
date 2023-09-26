d29bac8d89fbfa6ae81299ec1447ca72df0da7d8
hagarSamy <hagarsami63@gmail.com>
2023-09-25 17:31:23 -0700

    capitalize it

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes each word depending on
+ * preceding string.
+ * @str: a pointer to a string
+ * Return: a pointer to the final string
+ */
+
+char *cap_string(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (str[i - 1] == ' ' || str[i - 1] == '\t' ||
+			str[i - 1] == '\n' || str[i - 1] == ',' ||
+			str[i - 1] == ';' || str[i - 1] == '.' ||
+			str[i - 1] == '!' || str[i - 1] == '?' ||
+			str[i - 1] == '\"' || str[i - 1] == '(' ||
+			str[i - 1] == ')' || str[i - 1] == '{' ||
+			str[i - 1] == '}')
+		{
+			if (str[i] >= 'a' && str[i] <= 'z')
+				str[i] = str[i] - ('a' - 'A');
+		}
+		i++;
+	}
+	return (str);
+}
