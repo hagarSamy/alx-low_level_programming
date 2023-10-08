a5732c24aad6a6b14d60b16321c1482bb767d19a
hagarSamy <hagarsami63@gmail.com>
2023-10-08 10:29:40 -0700

    malloc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * allocates memory using malloc
+ * @b: number of bytes to allocate
+ * Return: void
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *ptr = malloc(b);
+	if (ptr == NULL)
+		exit(98);
+	return ((void*)ptr);
+}
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,1 @@
+more with dynamic memory allocation
