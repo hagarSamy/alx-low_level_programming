94b606ec62ccfa109ef34be90508318d4b9fe9cb
hagarSamy <hagarsami63@gmail.com>
2023-10-08 11:48:15 -0700

    string cat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -19,20 +19,20 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (s2 == NULL)
 		s2 = "";
 
-	while(s1[l])
+	while (s1[l])
 		l++;
-	while(s2[r])
+	while (s2[r])
 		r++;
 
-	if(n >= r)
+	if (n >= r)
 		n = r;
 	p = malloc(sizeof(char) * l + n + 1);
-	if(p == NULL)
-	return(NULL);
-	for(i = 0; i < l; i++)
+	if (p == NULL)
+	return (NULL);
+	for (i = 0; i < l; i++)
 		p[i] = s1[i];
-	for(i = 0; i < n; i++)
+	for (i = 0; i < n; i++)
 		p[l + i] = s2[i];
 	p[l + n + 1] = '\0';
-	return(p);
+	return (p);
 }
