1c1bbbed4c2a1c4934471fe657ed8bf7c02b6906
hagarSamy <hagarsami63@gmail.com>
2023-10-11 13:50:35 -0700

    simple callback

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_name - prints a name
+ * @name: pointer to a name
+ * @f: pointer to a function
+ * Return: void
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL && f != NULL)
+		f(name);
+}
