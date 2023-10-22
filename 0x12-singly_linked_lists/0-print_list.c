a3430e0764c18b3501245d52ec3c46a4932a5de9
hagarSamy <hagarsami63@gmail.com>
2023-10-22 15:42:14 -0700

    To find the length and print elements of a singly LL

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,34 +1,31 @@
-#include "lists.h"
-#include <stddef.h>
-
+#include"lists.h"
 /**
- * print_list - prints all elements in a struct
- * @h: a pointer to a head
+ * print_list - print the data of the linked list
+ * @h: pointer to the first node of the list
  * Return: number of nodes
- */
+*/
 
 size_t print_list(const list_t *h)
 {
-	size_t n = 0;
+	const list_t *temp = h;
+	size_t count = 0;
 
 	if (h == NULL)
+	{
 		return (0);
+	}
 	else
 	{
-		while (h != NULL)
+		while (temp != NULL)
 		{
-			if (h->str == NULL)
-			{
-				printf("[0] (nil)\n");
-			}
+			if (temp->str == NULL)
+			printf("[0] (nil)\n");
 			else
-			{
-				printf("[%d] (%s)\n", h->len, h->str);
-			}
+			printf("[%d] %s\n", temp->len, temp->str);
 
-			h = h->next;
-			n++;
+			h = temp->next;
+			count++;
 		}
 	}
-	return (n);
+	return (count);
 }
