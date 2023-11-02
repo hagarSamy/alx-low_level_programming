7a9fe0df3b497303088aeb232252254b63d59e05
root <root@hager-pc.localdomain>
2023-11-03 01:09:23 +0200

    Creating a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <fcntl.h>
+/**
+ * create_file - creates a file with the given text
+ * @filename: The name of the file
+ * @text_content: The text to write to the file
+ * Return: 1 if the file was created successfully,
+ * -1 if the file could not be created
+ * -1 if filename is NULL or if write fails
+*/
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	ssize_t written;
+
+	if (filename == NULL)
+	return (-1);
+	fd = open(filename, O_TRUNC | O_WRONLY | 0600);
+	if (fd == -1)
+	return (-1);
+	if (text_content)
+	{
+		written = write(fd, text_content, strlen(text_content));
+		if (written == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+	close(fd);
+	return (1);
+}
