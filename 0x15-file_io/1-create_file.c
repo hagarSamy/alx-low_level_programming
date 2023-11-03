d8e11f8dd8eeb12a28e2015838a4e876eaf053ef
root <root@hager-pc.localdomain>
2023-11-03 23:38:01 +0200

    Creating a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,9 +21,9 @@ int create_file(const char *filename, char *text_content)
 	if (text_content)
 	{
 		while (text_content[len])
-    	{
-       		 len++;
-    	}
+		{
+	   		 len++;
+		}
 		written = write(fd, text_content, len);
 		if (written == -1 || written != len)
 			return (-1);
