d0b402355fb3f2d2c9b892684d7f32d837bb8f19
hagarSamy <hs1800@fayoum.edu.eg>
2024-01-03 22:19:32 +0200

    INserting to the hashtable

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,13 +11,23 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int idx;
-	hash_node_t *newNode;
+	hash_node_t *newNode, *current;
 
 	if (ht == NULL || key == NULL || value == NULL)
 		return (0);
 
 	idx = key_index((const unsigned char *)key, ht->size);
-
+	current = ht->array[idx];
+	while(current)
+	{
+		if(strcmp(current->key, key) == 0)
+		{
+			free (current->value);
+			current->value = strdup(value);
+			return (1);
+		}
+		current = current->next;
+	}
 	newNode =  (hash_node_t *)malloc(sizeof(hash_node_t));
 	if (newNode == NULL)
 		return (0);
