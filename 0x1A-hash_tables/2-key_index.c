3766126dd6a583a1a96ab1264f4ac9ce408f0cd1
hagarSamy <hs1800@fayoum.edu.eg>
2024-01-03 17:07:07 +0200

    A function for getting the index

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -11,7 +11,7 @@
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 	unsigned long int idx;
-	(void) size;
-	idx = hash_djb2(key);
+
+	idx = hash_djb2(key) % size;
 	return (idx);
 }
