d86e9f9ed3c452b56015860a935515ba8109fc0a
HagarSamy <hagarsami63@gmai.com>
2024-01-03 15:01:46 +0200

    My first hashTable initialization

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -2,20 +2,20 @@
 
 /**
  * hash_table_create - creates a hash table
- * @size - size of the array
+ * @size: size of the array
  * Return: a pointer to the newly created hash table or
  * NULL errror happened
 */
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    hash_table_t* table = (hash_table_t*)malloc(sizeof(hash_table_t));
+	hash_table_t *table = (hash_table_t *)malloc(sizeof(hash_table_t));
 
-    if (table == NULL)
-        return (NULL);
-    table->size = size;
-    table->array = (hash_node_t **)malloc(sizeof(hash_node_t*) * size);
+	if (table == NULL)
+		return (NULL);
+	table->size = size;
+	table->array = (hash_node_t **)malloc(sizeof(hash_node_t *) * size);
 
 
-    return (table);
+	return (table);
 }
