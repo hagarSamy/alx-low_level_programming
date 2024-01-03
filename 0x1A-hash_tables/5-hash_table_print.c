207e600165953417f6422b0e24cdc720abd61e26
hagarSamy <hs1800@fayoum.edu.eg>
2024-01-03 23:04:56 +0200

    Printing a hash table

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -3,28 +3,28 @@
 /**
  * hash_table_print - prints a hash table
  * @ht: the hash table to print
- * 
+ *
  * Return: void
 */
 void hash_table_print(const hash_table_t *ht)
 {
-    long unsigned int idx = 0;
+	unsigned long int idx = 0;
 	hash_node_t *current;
 
-    if (ht == NULL)
-        return;
-    printf("{");
-    while(idx < ht->size)
-    {
-	    current = ht->array[idx];
-	    while (current)
-	    {
-		    printf("'%s': '%s'", current->key, current->value);
-            if (current->next != NULL)
-                printf(", ");
-		    current = current->next;
-	    }
-        idx++;
-    }
-    printf("}\n");
+	if (ht == NULL)
+		return;
+	printf("{");
+	while (idx < ht->size)
+	{
+		current = ht->array[idx];
+		while (current)
+		{
+			printf("'%s': '%s'", current->key, current->value);
+			if (current->next != NULL)
+				printf(", ");
+			current = current->next;
+		}
+		idx++;
+	}
+	printf("}\n");
 }
