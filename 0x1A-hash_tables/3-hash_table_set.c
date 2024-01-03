0690d40378e66ca9446fdb40e5cb0df139970472
hagarSamy <hs1800@fayoum.edu.eg>
2024-01-03 20:14:10 +0200

    INserting to the hashtable

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -26,8 +26,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	newNode->value = strdup(value);
 	if (newNode->key == NULL || newNode->value == NULL)
 	{
-		free(newNode->key);
-		free(newNode->value);
 		free(newNode);
 		return (0);
 	}
