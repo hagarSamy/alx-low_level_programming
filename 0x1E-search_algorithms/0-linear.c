dc1fd56760e530733590dd3bf1b9ecfb8b9b42d8
HagarSamy <hs1800@fayoum.edu.eg>
2024-05-08 21:13:48 +0300

    back to the lovely C and kinear search

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -12,6 +12,8 @@
 int linear_search(int *array, size_t size, int value)
 {
     size_t i = 0;
+
+
     for(i = 0; i < size; i++)
     {
         printf("Value checked array[%lu] = [%d]\n", i, array[i]);
