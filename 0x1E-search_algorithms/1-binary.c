91102c83585b8c2064cd40da303133618674a5f9
HagarSamy <hs1800@fayoum.edu.eg>
2024-05-09 02:22:23 +0300

    binary search

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -19,6 +19,7 @@ int binary_search(int *array, size_t size, int value)
 	while  (l <= r)
 	{
 		size_t i;
+
 		printf("Searching in array: ");
 		for (i = l; i <= r; i++)
 		{
@@ -33,17 +34,11 @@ int binary_search(int *array, size_t size, int value)
 		mid = (l + r) / 2;
 
 		if (array[mid] == value)
-		{
 			return (mid);
-		}
 		else if (array[mid] < value)
-		{
 			l = mid + 1;
-		}
 		else
-		{
 			r = mid - 1;
-		}
 	}
 	return (-1);
 }
